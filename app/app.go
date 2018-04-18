7b89d0bb37c469625d7dfcb256d830d6c828d0f0
tanhaitao <tanhaitao@pdmi.cn>
2018-04-18 14:17:49 +0800

    add fot http test

--- app/app.go
+++ app/app.go
@@ -2,14 +2,26 @@ package app
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 )
 
 type Config struct {
-	KubeConfig      string
+	KubeConfig string
 }
 
 func Run(config *Config) error {
 	fmt.Printf("app run ... \n")
 	fmt.Printf("config is %v \n", config)
+	http.HandleFunc("/hello", HelloServer)
+	err := http.ListenAndServe(":12345", nil)
+	if err != nil {
+		fmt.Printf("ListenAndServe: %v \n", err)
+	}
 	return nil
+}
+
+// hello world, the web server
+func HelloServer(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "hello, world!\n")
 }
